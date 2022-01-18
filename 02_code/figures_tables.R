723322a9d736e6165f00f70233b6189c394afaef
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2022-01-18 20:11:59 +0000

    ITN type and resistance
     - adding HPC results
     - fixing ITN data
    - changing ITN pyrethroid vs. pbo and insecticide resistance values
    - adding inputs for ITN type and resistance

--- 02_code/figures_tables.R
+++ 02_code/figures_tables.R
@@ -13,7 +13,8 @@ library(LaCroixColoR)
 
 # devtools::install_github('mrc-ide/malariasimulation@dev', force=TRUE)
 # devtools::install_github('johannesbjork/LaCroixColoR')
-knitr::opts_knit$set(message=FALSE, warning=FALSE)
 
 # Look here: https://github.com/htopazian/rtss_malariasimulation
 # find ideas in rmarkdowns for figures and insert code if helpful
+
+dat <- readRDS("M:/Hillary/GF-RTSS-CE/03_output/rtss_raw.rds")
