ef225ad79786d46f0c68a12c296fe757e7af732e
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2022-02-01 15:15:35 +0000

    PR ITN country distribution
    - adding script and figure

--- 02_code/figures_tables.R
+++ 02_code/figures_tables.R
@@ -32,11 +32,7 @@ seas_name <- 'seasonal'
 seasonality <- list(c(0.285505,-0.325352,-0.0109352,0.0779865,-0.132815,0.104675,-0.013919))
 s2 <- crossing(seasonality, seas_name)
 
-seas_name <- 'perennial'
-seasonality <- list(c(0.2852770,-0.0248801,-0.0529426,-0.0168910,-0.0216681,-0.0242904,-0.0073646))
-s3 <- crossing(seasonality, seas_name)
-
-stable <- rbind(s1, s2, s3)
+stable <- rbind(s1, s2)
 
 find_peak <- function(seasonality, seas_name){
   month <- 365/12
@@ -68,16 +64,17 @@ find_peak <- function(seasonality, seas_name){
 output <- map2_dfr(stable$seasonality, stable$seas_name, find_peak)
 
 baseline <- dat %>%
-  filter(ITNuse==0, RTSS=='none', ITN=='pyr') %>%
+  filter(ITNuse==0 &  RTSS=='none' &  ITN=='pyr' &
+           seasonality != 'perennial' & ITNboost == 0 & (seasonality=="seasonal" & SMC==0 | seasonality=="highly seasonal" & SMC==0.85)) %>%
   mutate(inc_month=n_inc_clinical_0_1825/n_0_1825, na.rm=T) %>%
   filter(month %in% seq(1,12,1))
-
 
 ggplot(data=baseline) +
   geom_line(data=baseline, aes(x=month, y=inc_month, color=as.factor(pfpr))) +
   geom_vline(data=output[output$intervention=='smc',], aes(xintercept=month, alpha='SMC'), lty=2, color='red') +
   geom_vline(data=output[output$intervention=='sv',], aes(xintercept=month, alpha='RTS,S'), lty=2, color='blue') +
   labs(title='distribution of SMC and seasonal RTS,S; ages 0-5 years',
+       caption='highly seasonal has SMC implemented at baseline',
        x="Timesteps (month)",
        y="Monthly clinical incidence",
        color = 'PfPR') +
@@ -93,7 +90,41 @@ ggsave('./03_output/seasonality.pdf', width=6, height=6)
 
 
 # seasonality II ---------------------------------------------------------------
-output <- dat %>% filter(SMC == 0.85 & RTSS == 'SV' & ITN == 'pyr' & ITNuse == 0.5 & resistance == 0 & ITNboost == 0)
+output <- dat %>% filter(SMC == 0.85 & RTSS == 'SV' & ITN == 'pyr' & ITNuse == 0.5 & resistance == 0 & ITNboost == 0 )%>%
+  filter(seasonality == 'highly seasonal' & pfpr == 0.4)
+
+# look at one file
+interventiontime <- output  %>%
+  select(smc_timesteps, rtss_mass_timesteps, bednet_timesteps) %>% distinct()
+
+SMCtime <- do.call(cbind.data.frame, interventiontime$smc_timesteps)
+colnames(SMCtime) <- "smc"
+
+RTSStime <- do.call(cbind.data.frame, interventiontime$rtss_mass_timesteps)
+colnames(RTSStime) <- "rtss"
+
+ITNtime <- do.call(cbind.data.frame, interventiontime$bednet_timesteps)
+colnames(ITNtime) <- "itn"
+
+
+ggplot(data = output) +
+  geom_line(aes(x=month, y=p_inc_clinical_0_1825/n_0_1825), alpha = 0.8) +
+  geom_vline(data = SMCtime, aes(xintercept=smc/(365/12), alpha='SMC'), lty=2, color='red') +
+  geom_vline(data = RTSStime, aes(xintercept=rtss/(365/12), alpha='RTS,S dose 3'), lty=2, color='blue') +
+  geom_vline(data = ITNtime, aes(xintercept=itn/(365/12), alpha='ITN'), lty=2, color='green') +
+  labs(x='month', y='clinical incidence (month), 0-5 years', subtitle = "highly seasonal, PfPR 0.4") +
+  scale_x_continuous(breaks = seq(0,20*12,12)) +
+  coord_cartesian(xlim = c(0,20*12)) +
+  scale_alpha_manual(values = c(rep(1,3))) +
+  guides(alpha = guide_legend(title = 'intervention',
+                              override.aes = list(color = c('green','blue','red')))) +
+  theme_classic()
+
+ggsave('./03_output/seasonalityII.pdf', width=10, height=4)
+
+
+# one year zoomed
+output <- dat %>% filter(SMC == 0.85 & RTSS == 'SV' & ITN == 'pyr' & ITNuse == 0.5 & resistance == 0 & ITNboost == 0 )
 
 SMCtime <- output %>% select(smc_timesteps, seasonality, pfpr) %>%
   group_by(smc_timesteps, seasonality, pfpr) %>%
@@ -134,7 +165,7 @@ ggplot(data = output) +
                               override.aes = list(color = c('green','blue','red')))) +
   theme_classic()
 
-ggsave('./03_output/seasonalityII.pdf', width=10, height=5)
+ggsave('./03_output/seasonalityIII.pdf', width=10, height=5)
 
 
 # bed nets and resistance ------------------------------------------------------
