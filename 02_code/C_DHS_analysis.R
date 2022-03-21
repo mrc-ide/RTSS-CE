c5e8375535f3301f4289a0aaa0cd782e2e521b36
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2022-03-21 18:35:39 +0000

    DHS Nigeria 2018 and MAP 2018 maps

--- 02_code/C_DHS_analysis.R
+++ 02_code/C_DHS_analysis.R
@@ -4,8 +4,15 @@ library(malariaAtlas)
 library(raster)
 library(sf)
 library(tidyverse)
+library(netz)
+library(survey)
+library(broom)
 
 # MAP data ---------------------------------------------------------------------
+# ITNs #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+# get MAP data from netz package and from PfPR tables
+nets_data <- netz::prepare_data()
+
 PR <- read_csv('./01_data/00_PfPR_table_Global_admin0_2000-2019.csv') %>%
   filter(Year == 2019) %>% arrange(PfPR_median) %>%
   mutate(SSA = case_when(Name_0 %in% nets_data$Name ~ 1,
@@ -47,6 +54,39 @@ SSA_ITN <- SSA_ITN0 %>% full_join(SSA_ITN1)
 saveRDS(SSA_ITN, './03_output/MAP_ITN.rds')
 
 
+# TRAVEL TIMES #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+# download shapefile and raster from MAP
+shp <- malariaAtlas::getShp(ISO = "NGA", admin_level = "admin1")
+plot(shp)
+
+traveltime <- malariaAtlas::getRaster(
+  surface = 'Global travel time to healthcare map with access to motorized transport',
+  shp = shp)
+malariaAtlas::autoplot_MAPraster(traveltime)
+
+
+# transforming to UTM 32N, to allow for creation of buffers in meters
+dhs_map <- st_as_sf(dat_start, coords = c("LONGNUM", "LATNUM"), crs = 4326) # all DHS coords in WGS 1984
+dhs_map <- st_transform(dhs_map %>% dplyr::select(CLUSTER) %>% dplyr::distinct(.keep_all=T), crs = 32632)
+plot(dhs_map)
+
+# extract travel times around cluster points, with a 1km buffer
+travel <- raster::extract(traveltime, dhs_map, buffer=1000, fun=mean)
+
+dhs_map <- cbind(dhs_map, travel)
+plot(dhs_map$travel)
+
+ggplot(dhs_map %>% filter(!is.na(travel))) +
+  geom_sf(aes(color=travel)) +
+  scale_color_distiller(palette = 'Spectral')
+
+# PfPR #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+# get MAP shapefiles for SSA admin1
+PfPR <- malariaAtlas::getRaster(
+  surface = 'Plasmodium falciparum PR2 - 10 version 2020',
+  year = 2018,
+  shp = shp)
+malariaAtlas::autoplot_MAPraster(PfPR)
 
 
 # DHS data ---------------------------------------------------------------------
@@ -76,7 +116,7 @@ vars_kr <- c("caseid", "v001", "v002", "v003", "midx", "v005", "b5",
 questions_kr <- search_variables(datasets_kr$FileName, variables = vars_kr)
 
 downloads_pr <- get_datasets(datasets_pr$FileName)
-vars_pr <- c("hhid", "hv001", "hv002", "hvidx", "hv005", "hv103", "hml12", "hc1", "hml35")
+vars_pr <- c("hhid", "hv001", "hv002", "hvidx", "hv005", "hv023", "hv103", "hml12", "hc1", "hml35")
 questions_pr <- search_variables(datasets_pr$FileName, variables = vars_pr)
 
 
@@ -101,25 +141,22 @@ extract_bound_pr <-
 saveRDS(extract_bound_kr,  file = "./03_output/DHS_nets_dtp3_KR.RDS")
 saveRDS(extract_bound_pr,  file = "./03_output/DHS_nets_dtp3_PR.RDS")
 
-
-
 # read in
 extract_bound_kr <- readRDS("./03_output/DHS_nets_dtp3_KR.RDS")
 extract_bound_pf <- readRDS("./03_output/DHS_nets_dtp3_PR.RDS")
 
 
-# First filter to children aged 1 and 2 (and alive) in Children's recode, then link person's recode. Then keep those who slept in house last night.
-
+# first filter to children aged 1 and 2 (and alive) in children's recode, then link person's recode. Then keep those who slept in house last night.
 dat_start <- extract_bound_kr %>%
   filter(b5 == 1, b8 %in% c(1,2)) %>%
   left_join(extract_bound_pr, by = c("v001" = "hv001", "v002" = "hv002", "v003" = "hvidx",
                                      "CLUSTER", "ALT_DEM", "LATNUM", "LONGNUM", "ADM1NAME",
                                      "DHSREGNA", "SurveyId")) %>%
-  filter(hv103 == 1, hml12 != 9, v459 != 9)
+  filter(hv103 == 1, hml12 != 9, v459 != 9) %>%
+  mutate(wt = v005/1e6)
 
-
-# select either "admin1", "region", or "country". Only need country
-
+# summarize data by admin1
+# select either "admin1", "region", or "country"
 level <- 'admin1'
 
 if (level == "admin1"){
@@ -135,26 +172,31 @@ if (level == "admin1"){
 # Summarise at selected level
 dat <- dat_start %>%
   group_by_at(grouping) %>%
-  summarise(num_children = sum(v005/1e6),
+  # sum women's individual sample weight and take the mean interview date
+  summarise(num_children = sum(v005 / 1e6),
             date_vacc_survey_cmc = mean(v008))
 
 dat_DTP_card <- dat_start %>%
   group_by_at(grouping) %>%
+  # received dpt3 (on vax card or reported by mother and health card is seen)
   filter((h7 %in% c(1, 3)) | (h1 == 1 & h7 == 2)) %>%
   summarise(num_DTP3_card = sum(v005 / 1e6))
 
 dat_DTP_report <- dat_start %>%
   group_by_at(grouping) %>%
+  # received dpt3 (reported by mother but health card not seen)
   filter(h7 == 2 & h1 != 1) %>%
   summarise(num_DTP3_report = sum(v005 / 1e6))
 
 dat_measles_card <- dat_start %>%
   group_by_at(grouping) %>%
+  # received measles1 (on vax card or reported by mother and health card is seen)
   filter((h9 %in% c(1, 3)) | (h1 == 1 & h9 == 2)) %>%
   summarise(num_measles_card = sum(v005 / 1e6))
 
 dat_measles_report <- dat_start %>%
   group_by_at(grouping) %>%
+  # received measles1 (reported by mother but health card not seen)
   filter(h9 == 2 & h1 != 1) %>%
   summarise(num_measles_report = sum(v005 / 1e6))
 
@@ -200,6 +242,7 @@ dat_vaccine_no_itn_no <- dat_start %>%
   filter(h7 == 0 & hml12 %in% c(0,3)) %>%
   summarise(vac_n_itn_n = sum(v005/1e6))
 
+# join all vars into single dataset
 dat_all <- left_join(dat, dat_DTP_card, by = grouping) %>%
   left_join(dat_DTP_report, by = grouping) %>%
   left_join(dat_measles_card, by = grouping) %>%
@@ -228,9 +271,64 @@ dat_all$countrycode <- substr(dat_all$SurveyId, 1, 2)
 # remove SurveyId
 dat_all <- dat_all %>% dplyr::select(-c(SurveyId))
 
-dat_all <- dat_all %>%
-  dplyr::select(c(countrycode, date_net_survey_cmc, date_treat_survey_cmc,
-                  date_vacc_survey_cmc, everything()))
-
+# save to .csv
 write_csv(dat_all, paste0("./03_output/combined_DHS_data_", level, ".csv"))
+
+dhs_map <- cbind(dhs_map, travel)
+
+# add coordinates
+dat_all <- shp %>% st_as_sf() %>% dplyr::select(name_1) %>%
+  mutate(name_1 = tolower(name_1)) %>%
+  left_join(dat_all %>% mutate(ADM1NAME = case_when(ADM1NAME=='FCT ABUJA'~'abuja',
+                                                    ADM1NAME=='NASARAWA'~'nassarawa',
+                                                    TRUE ~ ADM1NAME),
+                               ADM1NAME = tolower(ADM1NAME)), by = c('name_1'='ADM1NAME'))
+
+
+
+# save to .rds
+saveRDS(dat_all, paste0("./03_output/combined_DHS_data_", level, ".rds"))
+
+
+# inspect individual level DHS data
+# look for associations with DPT3, ITN, travel time
+# make variables for DPT3 and ITN status
+dat_start <- dat_start %>%
+  mutate(dpt3 = case_when(h7 %in% c(1, 2, 3) ~ 1,
+                          h7 == 0 ~ 0,
+                          TRUE ~ NA_real_),
+         itn = case_when(hml12 %in% c(1,2) ~ 1,
+                         hml12 %in% c(0,3) ~ 0,
+                         TRUE ~ NA_real_)) %>%
+  left_join(dhs_map, by = 'CLUSTER')
+
+DHSdesign <- survey::svydesign(id = ~CLUSTER, strata = ~hv023, weights = ~wt, data = dat_start)
+
+vars <- c('dpt3','itn','travel')
+
+
+# add mean travel
+plotd <- dat_start %>% group_by(CLUSTER) %>%
+  summarize(dpt3 = mean(dpt3, na.rm=T),
+         itn = mean(itn, na.rm=T)) %>%
+  left_join(dhs_map, by='CLUSTER')
+
+ggplot(data = plotd, aes(x=travel, y=dpt3)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  coord_cartesian(ylim = c(0,1)) + theme_classic()
+
+ggplot(data = plotd, aes(x=travel, y=itn)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  coord_cartesian(ylim = c(0,1)) + theme_classic()
+
+
+m <- svyglm(dpt3 ~ travel, DHSdesign, family=quasibinomial("identity")) # can't converge - maybe try categorizing travel
+
+m <- svyglm(dpt3 ~ travel, DHSdesign, family=binomial("logit"))
+cbind(tidy(m), confint(m))
+
+m <- svyglm(itn ~ travel, DHSdesign, family=binomial("logit"))
+cbind(tidy(m), confint(m))
 
