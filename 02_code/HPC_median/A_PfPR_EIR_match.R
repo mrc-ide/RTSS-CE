fe5575dfa6be93fb380c2f8985a29604917907c6
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2022-06-07 14:10:41 +0100

    re-org files

--- 02_code/HPC_median/A_PfPR_EIR_match.R
+++ 02_code/HPC_median/A_PfPR_EIR_match.R
@@ -91,27 +91,6 @@ t <- obj$enqueue_bulk(combo, PRmatch)
 t$status()
 
 
-# DHS matches ------------------------------------------------------------------
-ITNuse <- c(0.22, 0.36, 0.39, 0.44, 0.58, 0.60) # runs for admin1
-combo <- crossing(stable, init_EIR, ITN, ITNuse) %>% mutate(name = paste0('EIRadmin1', "_", seas_name, "_", init_EIR, "_", ITNuse))
-
-combo <- combo %>%
-  filter(!(seas_name == 'seasonal' & init_EIR > 70)) %>%
-  filter(!(seas_name == 'perennial' & init_EIR > 100)) %>%
-  filter(!(ITNuse %in% c(0.22, 0.44) & seas_name != 'seasonal')) %>%
-  filter(!(ITNuse %in% c(0.36, 0.58) & seas_name != 'highly seasonal')) %>%
-  filter(!(ITNuse %in% c(0.39, 0.60) & seas_name != 'perennial'))
-
-
-combo <- combo %>% mutate(f = paste0("./03_output/PR_EIR/",combo$name,".rds")) %>%
-  mutate(exist=case_when(file.exists(f) ~ 1, !file.exists(f) ~ 0)) %>%
-  filter(exist==0) %>%
-  select(-f, -exist)
-
-t <- obj$enqueue_bulk(combo, PRmatch_u5)
-t$status()
-
-
 # Results ----------------------------------------------------------------------
 library(tidyverse)
 library(data.table)
@@ -137,25 +116,6 @@ p
 
 ggsave('C:/Users/htopazia/OneDrive - Imperial College London/Github/GF-RTSS-CE/03_output/prev_EIR_plot_ITN.pdf', height=7, width=7)
 
-
-# < admin1s ----
-files <- list.files(path = "./03_output/PR_EIR", pattern = "EIRadmin1_", full.names = TRUE)
-dat_list <- lapply(files, function (x) readRDS(x))
-EIR_prev <-  do.call("rbind", dat_list) %>% as_tibble() %>%
-  mutate(init_EIR = as.numeric(init_EIR),
-         prev = as.numeric(prev),
-         scenario = paste(seas_name, ITN, ITNuse, sep = '_'))
-
-q <- ggplot(data=EIR_prev) +
-  geom_point(aes(x=init_EIR, y=prev), color='black') +
-  stat_smooth(aes(x=init_EIR, y=prev), color='red', method = 'gam', n=1000) +
-  labs(x='initial EIR', y=expression(paste(italic(Pf),"PR"[5-59]))) +
-  facet_wrap('scenario', nrow=3, ncol=2) +
-  theme_classic()
-
-q
-
-ggsave('C:/Users/htopazia/OneDrive - Imperial College London/Github/GF-RTSS-CE/03_output/prev_EIR_plot_ITN_admin1.pdf', height=7, width=7)
 
 
 # Match EIR and PfPR -----------------------------------------------------------
@@ -192,34 +152,3 @@ summary(match)
 # save for other HPC runs
 saveRDS(match, "./02_code/HPC/EIRestimates.rds")
 
-
-# < admin1s ----
-# extract points from stat_smooth
-q2 <- ggplot_build(q)
-q2 <- q2$data[[2]]
-
-pnames <- EIR_prev %>% select(scenario) %>% distinct() %>%
-  arrange(scenario) %>% mutate(PANEL=row_number())
-
-q2 <- q2 %>% as_tibble() %>% select(x,y,PANEL) %>% rename(init_EIR=x, pred=y) %>%
-  mutate(PANEL = as.numeric(PANEL)) %>%
-  left_join(pnames, by='PANEL')
-
-# Pre-intervention baseline PfPR
-PfPR <- as_tibble_col(c(0.05, 0.07, 0.12, 0.18, 0.39, 0.40), column_name="pfpr")
-
-# match via stat_smooth predictions
-match <- PfPR %>%
-  fuzzyjoin::difference_left_join(q2, by=c("pfpr"="pred"),
-                                  max_dist=1, distance_col="dist") %>%
-  group_by(pfpr, scenario) %>% slice_min(dist)
-
-match <- match %>% rename(starting_EIR = init_EIR) %>%
-  separate(col = scenario, into = c("seas_name", "ITN", "ITNuse"), sep="_") %>%
-  mutate(ITNuse = as.numeric(ITNuse)) %>%
-  select(pfpr, starting_EIR, seas_name, ITN, ITNuse)
-
-summary(match)
-
-# save for other HPC runs
-saveRDS(match, "./02_code/HPC/EIRestimates_admin1.rds")
